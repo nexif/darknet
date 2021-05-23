c062287e7d29be7f00271a23ae63a7475b81bcfa
Nexif <m.kolcon@protonmail.com>
2021-05-23 21:28:37 +0200

    1st

---
+++ yolov4_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+classes=4
+max_batches=8000
+batch=64
+subdivisions=16
+width=416
+height=416
+channels=3
+momentum=0.949
+decay=0.0005
+learning_rate=0.001
+steps= (6400,7200)
+scales=(0.1,0.1)
